124995bda7cfd388feb3348db7f075018084e4af
Matt <60686099+conjuur@users.noreply.github.com>
2020-11-28 11:51:44 -0800

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,39 @@
+'''
+Copyright (C) 2020 Conjuur
+conjuur@gmail.com
+
+Created by Matthew Myers
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import bpy
+
+bl_info = {
+    "name":        "ImageVoodoo",
+    "description": "Image handling tools for Blender",
+    "author":      "Matthew Myers",
+    "version":     (1, 0, 0),
+    "blender":     (2, 91, 0),
+    "location":    "View 3D > Header",
+    "doc_url":     "",
+    "tracker_url": "",
+    "category":    "3D View",
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
